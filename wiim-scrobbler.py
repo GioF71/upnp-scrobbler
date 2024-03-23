712fd7025fe8d6816bad5cbe980fabccdf9c8fed
Giovanni Fulco <giovanni.fulco@gmail.com>
2024-03-23 19:12:39 +0100

    Initial work

---
+++ wiim-scrobbler.py
@@ -0,0 +1,313 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# pylint: disable=invalid-name
+
+import asyncio
+import json
+import sys
+import time
+import xmltodict
+from typing import Any, Optional, Sequence, Union
+
+# from didl_lite import didl_lite
+
+# from async_upnp_client.advertisement import SsdpAdvertisementListener
+from async_upnp_client.aiohttp import AiohttpNotifyServer, AiohttpRequester
+from async_upnp_client.client import UpnpDevice, UpnpService, UpnpStateVariable
+from async_upnp_client.client_factory import UpnpFactory
+# from async_upnp_client.const import AddressTupleVXType, SsdpHeaders
+from async_upnp_client.exceptions import UpnpResponseError
+from async_upnp_client.profiles.dlna import dlna_handle_notify_last_change
+# from async_upnp_client.search import async_search as async_ssdp_search
+from async_upnp_client.utils import get_local_ip
+
+NAMESPACES = {
+    "didl_lite": "urn:schemas-upnp-org:metadata-1-0/DIDL-Lite/",
+    "dc": "http://purl.org/dc/elements/1.1/",
+    "upnp": "urn:schemas-upnp-org:metadata-1-0/upnp/",
+    "xsi": "http://www.w3.org/2001/XMLSchema-instance",
+    "song": "www.wiimu.com/song/",
+    "custom": "www.linkplay.com/custom/",
+}
+
+key_title: str = "dc:title"
+key_subtitle: str = "dc:subtitle"
+key_artist: str = "upnp:artist"
+key_album: str = "upnp:album"
+key_duration: tuple[str, str] = ["res", "@duration"]
+
+
+def duration_str_to_sec(duration: str) -> float:
+    # print(f"duration_str_to_sec duration [{duration}]")
+    millis: str = duration[len(duration) - 3:]
+    seconds: str = duration[len(duration) - 6:len(duration) - 4]
+    minutes: str = duration[len(duration) - 9:len(duration) - 7]
+    hours: str = duration[0:len(duration) - 10]
+    # print(f"duration_str_to_sec millis: [{millis}] seconds: [{seconds}] "
+    #       f"minutes: [{minutes}] hours: [{hours}]")
+    result: float = (float(int(millis) / 1000.0)
+                     + float(int(seconds))
+                     + float(int(minutes) * 60.0)
+                     + float(int(hours) * 3600.0))
+    # print(f"Result duration: [{result}]")
+    return result
+
+
+class CurrentSong:
+
+    def __init__(self):
+        # keep track of creation time
+        self._playback_start: float = time.time()
+
+    @property
+    def playback_start(self) -> float:
+        return self._playback_start
+
+    @property
+    def title(self) -> str:
+        return self._title
+
+    @title.setter
+    def title(self, value: str):
+        self._title: str = value
+
+    @property
+    def subtitle(self) -> str:
+        return self._subtitle
+
+    @subtitle.setter
+    def subtitle(self, value: str):
+        self._subtitle: str = value
+
+    @property
+    def artist(self) -> str:
+        return self._artist
+
+    @artist.setter
+    def artist(self, value: str):
+        self._artist: str = value
+
+    @property
+    def album(self) -> str:
+        return self._album
+
+    @album.setter
+    def album(self, value: str):
+        self._album: str = value
+
+    @property
+    def duration(self) -> float:
+        return self._duration
+
+    @duration.setter
+    def duration(self, value: float):
+        self._duration: str = value
+
+
+the_current_song: CurrentSong = None
+
+items = {}
+
+pprint_indent = 4
+
+event_handler = None
+playing = False
+
+
+async def create_device(description_url: str) -> UpnpDevice:
+    """Create UpnpDevice."""
+    timeout = 60
+    non_strict = True
+    requester = AiohttpRequester(timeout)
+    factory = UpnpFactory(requester, non_strict=non_strict)
+    return await factory.async_create_device(description_url)
+
+
+def get_timestamp() -> Union[str, float]:
+    """Timestamp depending on configuration."""
+    return time.time()
+
+
+def service_from_device(
+      device: UpnpDevice,
+      service_name: str) -> Optional[UpnpService]:
+    """Get UpnpService from UpnpDevice by name or part or abbreviation."""
+    for service in device.all_services:
+        part = service.service_id.split(":")[-1]
+        abbr = "".join([c for c in part if c.isupper()])
+        if service_name in (service.service_type, part, abbr):
+            return service
+    return None
+
+
+def maybe_scrobble(current_song: CurrentSong):
+    now: float = time.time()
+    if current_song.duration:
+        elapsed: float = now - current_song.playback_start
+        # print(f"maybe_scrobble elapsed: [{elapsed}]")
+        if elapsed >= 240.0 or elapsed >= (current_song.duration / 2.0):
+            print(f"maybe_scrobble we can scrobble [{current_song.title}] "
+                  f"from [{current_song.album}] "
+                  f"by [{current_song.artist}]")
+        else:
+            print(f"maybe_scrobble cannot scrobble [{current_song.title}] "
+                  f"from [{current_song.album}] "
+                  f"by [{current_song.artist}], "
+                  f"elapsed: [{elapsed}] duration: [{current_song.duration}]")
+    else:
+        print("Song has no duration, cannot scrobble")
+
+
+def on_event(
+      service: UpnpService,
+      service_variables: Sequence[UpnpStateVariable]) -> None:
+    """Handle a UPnP event."""
+    obj = {
+        "timestamp": get_timestamp(),
+        "service_id": service.service_id,
+        "service_type": service.service_type,
+        "state_variables": {sv.name: sv.value for sv in service_variables},
+    }
+    global playing
+    global items
+    global the_current_song
+    # special handling for DLNA LastChange state variable
+    if (len(service_variables) == 1 and
+            service_variables[0].name == "LastChange"):
+        last_change = service_variables[0]
+        dlna_handle_notify_last_change(last_change)
+    else:
+        for sv in service_variables:
+            # PAUSED, PLAYING, STOPPED, etc
+            # print(sv.name,sv.value)
+            # print(f"sv.name [{sv.name}] sv.value [{sv.value}]")
+            if sv.name == "TransportState":
+                print(f"TransportState = [{sv.value}]")
+                if sv.value == "PLAYING":
+                    playing = True
+                else:
+                    playing = False
+                    if sv.value == "STOPPED" and the_current_song:
+                        print("Scrobbling because: [STOPPED]")
+                        maybe_scrobble(the_current_song)
+                        the_current_song = None
+                    # when do we try to scrobble?
+                    # TRANSITIONING
+                    # if the_current_song:
+                    #    can_scrobble: bool = True
+                    #    if sv.value == "TRANSITIONING":
+                    #        print("transitioning -> can_scrobble")
+                    #        # can_scrobble = True
+                    #    # scrobble!
+                    #    if can_scrobble:
+                    #        maybe_scrobble(the_current_song)
+                    #        the_current_song = None
+            # Grab and print the metadata
+            if (sv.name == "CurrentTrackMetaData" or
+                    sv.name == "AVTransportURIMetaData"):
+                # Convert XML to beautiful JSON
+                items = xmltodict.parse(sv.value)["DIDL-Lite"]["item"]
+                # Print the entire mess
+                # print(json.dumps(items, indent=4))
+                if the_current_song and sv.name == "CurrentTrackMetaData":
+                    # scrobble!
+                    print(f"We want to scrobble because: [{sv.name}]")
+                    maybe_scrobble(the_current_song)
+                    the_current_song = None
+                # start creating a new CurrentSong instance
+                current_song: CurrentSong = CurrentSong()
+                current_song.title = items[key_title] if key_title in items else None
+                current_song.subtitle = items[key_subtitle] if key_subtitle in items else None
+                current_song.album = items[key_album] if key_album in items else None
+                current_song.artist = items[key_artist] if key_artist in items else None
+                duration_str: str = (items[key_duration[0]][key_duration[1]]
+                                     if key_duration[0] in items and key_duration[1] in items[key_duration[0]]
+                                     else None)
+                if duration_str:
+                    current_song.duration = duration_str_to_sec(duration_str)
+                # Print each item of interest
+                try:
+                    arttmp = items["upnp:albumArtURI"]
+                    art: str = None
+                    if isinstance(arttmp, dict):
+                        art = art["#text"]
+                    else:
+                        art = arttmp
+                    # print("Art:", art)
+                except Exception:
+                    pass
+
+                if (not the_current_song or
+                        current_song.title != the_current_song.title or
+                        current_song.subtitle != the_current_song.subtitle or
+                        current_song.artist != the_current_song.artist or
+                        current_song.album != the_current_song.album):
+                    print("Replacing the_current_song")
+                    the_current_song = current_song
+                    if not current_song.duration:
+                        print("No duration available, won't be able to scrobble!")
+                else:
+                    print("Sticking with the same current_song")
+
+
+async def subscribe(description_url: str, service_names: Any) -> None:
+    """Subscribe to service(s) and output updates."""
+    global event_handler  # pylint: disable=global-statement
+    device = await create_device(description_url)
+    # start notify server/event handler
+    source = (get_local_ip(device.device_url), 0)
+    server = AiohttpNotifyServer(device.requester, source=source)
+    await server.async_start_server()
+    # gather all wanted services
+    if "*" in service_names:
+        service_names = device.services.keys()
+    services = []
+    for service_name in service_names:
+        service = service_from_device(device, service_name)
+        if not service:
+            print(f"Unknown service: {service_name}")
+            sys.exit(1)
+        service.on_event = on_event
+        services.append(service)
+    # subscribe to services
+    event_handler = server.event_handler
+    for service in services:
+        try:
+            await event_handler.async_subscribe(service)
+        except UpnpResponseError as ex:
+            print(f"Unable to subscribe to {service}: {ex}")
+    s = 0
+    # keep the webservice running
+    while True:
+        await asyncio.sleep(10)
+        s = s + 1
+        if s >= 12:
+            await event_handler.async_resubscribe_all()
+            s = 0
+
+
+async def async_main() -> None:
+    """Async main."""
+    #  NOTICE!!!! #####################################
+    #  Your WiiM Mini's IP and port go here
+    device = "http://192.168.1.7:49152/description.xml"
+    service = ["AVTransport"]
+    await subscribe(
+        description_url=device,
+        service_names=service)
+
+
+def main() -> None:
+    """Set up async loop and run the main program."""
+    loop = asyncio.get_event_loop()
+    try:
+        loop.run_until_complete(async_main())
+    except KeyboardInterrupt:
+        if event_handler:
+            loop.run_until_complete(event_handler.async_unsubscribe_all())
+    finally:
+        loop.close()
+
+
+if __name__ == "__main__":
+    main()
