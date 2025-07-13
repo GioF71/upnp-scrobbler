93a74950fa95235bf4341e9d1108de3b41370e6f
Giovanni Fulco <giovanni.fulco@gmail.com>
2025-07-13 20:07:30 +0200

    #3 Improve last.fm authentication

--- upnp_scrobbler/constants.py
+++ upnp_scrobbler/constants.py
@@ -1,3 +1,31 @@
+from enum import Enum
+
+
+class Constants(Enum):
+    APP_NAME = "upnp-scrobbler"
+    LAST_FM = "last.fm"
+    LAST_FM_SESSION_KEY = "last_fm_session_key"
+
+
+class _ConfigParamData:
+
+    def __init__(self, key: str, default_value: any):
+        self.__key: str = key
+        self.__default_value: any = default_value
+
+    @property
+    def key(self) -> str:
+        return self.__key
+
+    @property
+    def default_value(self) -> any:
+        return self.__default_value
+
+
+class ConfigParam(Enum):
+    pass
+
+
 DEFAULT_DURATION_THRESHOLD: int = 240
 DEFAULT_DUMP_UPNP_DATA: bool = False
 DEFAULT_DUMP_EVENT_KEYS: bool = False
